2fbc8da68405c9229edfd19bf4b8dfc19db74fcf
gavinoconnor <gedmund@gmail.com>
2023-08-12 20:11:36 -0400

    append image after src is set

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const animalName = document.querySelector('.animal-name')
 
 // const animalSound = document.querySelector('.animal-sound')
 
-// Temporary animal array
-// Also need to incorporate sounds
+// Temporary animal array - need to query animals directly
+// to avoid , see docs
 const animalNames = [
-  'Cat', 'Dog', 'Elephant', 'Tiger', 'Lion',
-  'Bear', 'Eagle', 'Crow', 'Wolf', 'Fox', 
-  'Giraffe', 'Bull', 'Cow', 'Pig', 'Chicken', 
-  'Shark', 'Goldfish', 'Dolphin', 'Whale',
-  'Porcupine', 'Squirrel', 'Mouse', 'Bat', 
-  'Coyote', 'Rabbit', 'Chipmunk', 'Raccoon',
-  'Bobcat', 'Lynx', 'Otter']
-
+  'Cat', 'Dog', 'Elephant', 'Bunny', 'Lion',
+  // 'Bear', 'Eagle', 'Crow', 'Wolf', 'Fox', 
+  // 'Giraffe', 'Bull', 'Cow', 'Pig', 'Chicken', 
+  // 'Shark', 'Goldfish', 'Dolphin', 'Whale',
+  // 'Porcupine', 'Squirrel', 'Mouse', 'Bat', 
+  // 'Coyote', 'Rabbit', 'Chipmunk', 'Raccoon',
+  // 'Lynx', 'Otter'
+]
+// TO DO: convert animalNames into
+// an array of objects so you can 
+// add sounds, etc
 function getRandomAnimalName() {
   const randomIndex = Math.floor(Math.random() * animalNames.length);
   return animalNames[randomIndex];
@@ -35,11 +38,8 @@ function handleClick() {
   // Create an img element
   const animalImg = document.createElement('img');
   animalImg.className = 'animal-img'; // Set class to apply CSS styling
-  animalImg.alt = randomAnimalName; // Set alt attribute to the animal name
-  // Append the img element to the container
-  imageContainer.appendChild(animalImg);
+  animalImg.alt = randomAnimalName; // Set alt attribute to the animal name  
   // TO DO: abstract to a function outside the click handler
-
 
   const unsplashUrl = `https://api.unsplash.com/photos/random?query=${randomAnimalName}&w=400&client_id=myKey`;
   placeholderText.classList.add('hidden')
@@ -48,9 +48,9 @@ function handleClick() {
     .then(response => response.json())
     .then(data => {
       animalImg.src = data.urls.small
+      imageContainer.appendChild(animalImg) // append to DOM after src is set
     })
     .catch(error => {
-      // Handle errors
       console.error('Error fetching image', error)
       // Remove the img element and show the placeholder text again
       animalImg.remove();
