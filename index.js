b88de4e4813cf32137adf6117f5930cf1c81fa0d
gavinoconnor <gedmund@gmail.com>
2023-09-12 16:50:39 -0400

    update handleClick function

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-import animals from "./animals.js"
+import icons from "./animals.js"
 
 const btn = document.getElementById('btn')
-// const imageContainer = document.getElementById('image-container')
-// const placeholderText = document.getElementById('placeholder-text')
-// const animalName = document.querySelector('.animal-name')
+const iconEl = document.getElementById('icon')
+const animalNameEl = document.getElementById('animal-name')
+const placeholderText = document.getElementById('placeholder-text')
 
-// import animal icons from Font Awesome
-console.log(animals)
-// store them in an array
-// render random animal on click
-
-// function getRandomAnimalName() {
-//   const randomIndex = Math.floor(Math.random() * animalNames.length);
-//   return animalNames[randomIndex];
-// }
+let randomIcon
 
 function handleClick() {
-  console.log("click")
+  randomIcon = icons[Math.floor(Math.random() * icons.length)]
+  
+  // Convert class list to an array, find fa-animalName
+  const currentIconName = iconEl.classList.value.split(' ').find(cls => {
+    return cls.startsWith('fa-') && cls !== 'fa-light' && !cls.includes('x')
+  })
 
+  if (currentIconName) {
+    iconEl.classList.remove(currentIconName)
+    placeholderText.classList.add('hidden')
+  }
+
+  // Set the new animal and name
+  iconEl.classList.remove('hidden')
+  iconEl.classList.add(`fa-${randomIcon}`)
+  animalNameEl.textContent = randomIcon.toUpperCase()
 }
 
 
