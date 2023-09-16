71315d58acb383b1e99e9b0f1c8cacb0ec65e558
gavinoconnor <gedmund@gmail.com>
2023-09-16 13:54:49 -0400

    add listener to prevent double-taps

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const iconEl = document.getElementById('icon')
 const animalNameEl = document.getElementById('animal-name')
 const placeholderText = document.getElementById('placeholder-text')
 
-let randomIcon
+const ICON_PREFIX = 'fa-'
 
 function handleClick() {
-  randomIcon = icons[Math.floor(Math.random() * icons.length)]
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)]
   
   // Convert class list to an array, find fa-animalName
   const currentIconName = iconEl.classList.value.split(' ').find(cls => {
-    return cls.startsWith('fa-') && cls !== 'fa-light' && !cls.includes('x')
+    return cls.startsWith(ICON_PREFIX) && 
+           cls !== 'fa-light' && 
+           !cls.includes('x')
   })
 
   if (currentIconName) {
@@ -22,9 +24,21 @@ function handleClick() {
 
   // Set the new animal and name
   iconEl.classList.remove('hidden')
-  iconEl.classList.add(`fa-${randomIcon}`)
+  iconEl.classList.add(`${ICON_PREFIX}${randomIcon}`)
   animalNameEl.textContent = randomIcon.toUpperCase()
 }
 
-
 btn.addEventListener('click', handleClick)
+
+// Prevent double-taps
+let lastTouchEnd = 0
+
+document.addEventListener('touchend', event => {
+  const now = Date.now()
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault()
+  }
+  lastTouchEnd = now
+})
+
+
