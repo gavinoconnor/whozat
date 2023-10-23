c59811e3d4c653c527d900cc7f54e71fccc76a9c
gavinoconnor <gedmund@gmail.com>
2023-10-23 16:13:29 -0400

    add size property to icon

--- src/AnimalTile.js
+++ src/AnimalTile.js
@@ -1,12 +1,12 @@
 import './AnimalTile.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faOtter } from '@fortawesome/pro-regular-svg-icons'
+import { faOtter } from '@fortawesome/pro-light-svg-icons'
 
  export default function AnimalTile(props) {
   return (
     <div className="animal-tile">
       {/* <h2>{props.value}</h2> */}
-      <FontAwesomeIcon icon={faOtter}/>
+      <FontAwesomeIcon icon={faOtter} size="3x"/>
     </div>
   )
 }
