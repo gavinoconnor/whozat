e4030d782219a95737ba9410af9f647f13a40148
gavinoconnor <gedmund@gmail.com>
2023-10-25 17:33:24 -0400

    map fa-icon to name, display random animals

--- src/AnimalTile.js
+++ src/AnimalTile.js
@@ -1,12 +1,45 @@
 import './AnimalTile.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faOtter } from '@fortawesome/pro-light-svg-icons'
+import { 
+  faAlicorn, 
+  faBat, faBee, faBird, faBugs,
+  faCat, faCrab, faCow, faCrow, 
+  faDeer, faDinosaur, faDog, faDolphin, faDove, faDragon, faDuck, 
+  faElephant,
+  faFish, faFrog,
+  faHippo, faHorse, faLobster, faLocust,
+  faMonkey, faMosquito,
+  faNarwhal,
+  faOtter,
+  faPegasus, faPig,
+  faRabbit, faRaccoon, faRam,
+  faSheep, faShrimp, faSnake,
+  faTurtle,
+  faUnicorn,
+  faWhale, faWorm 
+ } from '@fortawesome/pro-light-svg-icons'
+
+ // name to icon mapping
+ const animalIcons = {
+    alicorn: faAlicorn,
+    bat: faBat, bee: faBee, bird: faBird, bugs: faBugs,
+    cat: faCat, crab: faCrab, cow: faCow, crow: faCrow, 
+    deer: faDeer, dinosaur: faDinosaur, dog: faDog, dolphin: faDolphin,
+    dove: faDove, dragon: faDragon, duck: faDuck, elephant: faElephant,
+    fish: faFish, frog: faFrog, hippo: faHippo, horse: faHorse, 
+    lobster: faLobster, locust: faLocust, monkey: faMonkey,
+    mosquito: faMosquito, narwhal: faNarwhal, otter: faOtter, 
+    pegasus: faPegasus, pig: faPig, rabbit: faRabbit, raccoon: faRaccoon,
+    ram: faRam, sheep: faSheep, shrimp: faShrimp, snake: faSnake, 
+    turtle: faTurtle, unicorn: faUnicorn, whale: faWhale, worm: faWorm
+ }
 
  export default function AnimalTile(props) {
+
   return (
-    <div className="animal-tile">
-      {/* <h2>{props.value}</h2> */}
-      <FontAwesomeIcon icon={faOtter} size="3x"/>
+    <div className="animal-tile" onClick={props.handleClick}>
+      <FontAwesomeIcon icon={animalIcons[props.value]} size="3x"/>
     </div>
   )
+
 }
--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
+import { useState } from 'react'
 import './App.css'
 import AnimalTile from './AnimalTile'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faOtter } from '@fortawesome/pro-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 
 function App() {
+
+  const animalNames = [
+    "alicorn", "bat", "bee", "bird", "bugs", "cat", "crab", "cow", "crow", "deer", 
+    "dinosaur", "dog", "dolphin", "dove", "dragon", "duck", "elephant", "fish", 
+    "frog", "hippo", "horse", "lobster", "locust", "monkey", "mosquito", "narwhal", 
+    "otter", "pegasus", "pig", "rabbit", "raccoon", "ram", "sheep", "shrimp", "snake", 
+    "spider", "squid", "squirrel", "turtle", "unicorn", "whale", "worm"
+  ]
+
+  const [shuffledAnimals, setShuffledAnimals] = useState(() => {
+    const allShuffled = shuffle(animalNames)
+    const selected = allShuffled.slice(0, 4)
+    return [...selected, ...selected, ...selected]
+  })
+
+  function shuffle(array) {
+    let shuffledArray = [...array]
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1))
+      // swap elements 
+      let temp = shuffledArray[i]
+      shuffledArray[i] = shuffledArray[randomIndex]
+      shuffledArray[randomIndex] = temp
+    }
+    return shuffledArray
+  }
+
+  function handleClick() {
+    console.log("click")
+  }
+  
   return (
     <div className="App">
-      {/* Title component */}
       <h2>WHOZAT!?</h2>
 
       <div className="wrapper">
-        {/* Game component */}
         <div className="tile-container">
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
-          <AnimalTile value="🤗"/>
+          {shuffledAnimals.map((animal, index) => (
+            <AnimalTile 
+              key={index} 
+              value={animal} 
+              handleClick={handleClick}
+            />
+          ))}
         </div>
       </div>
       {/* end wrapper */}
